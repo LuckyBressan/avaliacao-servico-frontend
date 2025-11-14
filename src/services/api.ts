bc5db882632d0230a97f8ca2078cf08e9928956d
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2025-11-13 22:14:37 -0300

    feat: updated to allow dynamic backend request path;

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://localhost:${import.meta.env.VITE_PORT_BACKEND}/avaliacao-servico/avaliacao-servico-backend/src/`,
+    baseURL: `http://localhost:${import.meta.env.VITE_PORT_BACKEND}/${import.meta.env.VITE_PATH_BACKEND}/src/`,
     headers: {
         'Content-Type': 'application/json'
     }
